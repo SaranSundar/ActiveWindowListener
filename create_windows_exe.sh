73e127e24a92a31eaa03f0f186e9f6a1df803cdf
saransundar <saransszg@gmail.com>
2020-04-30 00:00:24 -0500

    Works

--- create_windows_exe.sh
+++ create_windows_exe.sh
@@ -17,6 +17,6 @@ cp -r react-ui/build/static static/
 cp apis/mongo/mongoServer.config static/
 echo 'Building exe...'
 # --add-data 'icons/*.png;static/icons'
-pyinstaller --noupx --hidden-import='pkg_resources.py2_warn' --hidden-import='pynput' --add-data "templates;templates" --add-data "static;static" -y flair.py
+pyinstaller  -w --noupx --hidden-import='pkg_resources.py2_warn' --hidden-import='pynput' --add-data "templates;templates" --add-data "static;static" -y flair.py
 echo "Flair.exe created. Navigate to dist/ and double click flair.exe or run ./flair.exe in git-bash to launch the application. May take a couple seconds to launch"
 rm -rf *.spec
